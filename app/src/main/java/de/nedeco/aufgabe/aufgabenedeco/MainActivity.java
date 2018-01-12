37a5ae155a09171807cc39b1af6e121d11f3a7e4
LordSalkin <niklas@brendt.org>
2018-01-12 16:45:01 +0100

    Update MainActivity.java

--- app/src/main/java/de/nedeco/aufgabe/aufgabenedeco/MainActivity.java
+++ app/src/main/java/de/nedeco/aufgabe/aufgabenedeco/MainActivity.java
@@ -1,16 +1,21 @@
 package de.nedeco.aufgabe.aufgabenedeco;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -29,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<HashMap<String, String>> list = new ArrayList<>();
 
+    Context context = new ContextWrapper(this);
+
+    SharedPreferences sharedPref;
+
+    SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +47,18 @@ public class MainActivity extends AppCompatActivity {
             searchFilter = getResources().getString(R.string.none);
         }
 
+
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.animationin_lr, R.anim.animationout_lr);
         setContentView(R.layout.activity_main);
 
 
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+
         final ListView roetgenEventList = findViewById(R.id.roetgenEventList);
 
-        SwipeRefreshLayout refreshLayout = findViewById(R.id.swipeRefresh);
+        refreshLayout = findViewById(R.id.swipeRefresh);
 
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -58,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         try {
-            entries = new XmlParsing().execute("https://www.datefix.de/js/kalender/rss/5095").get();
+            entries = new XmlParsing().execute(sharedPref.getString("RSSLink",getResources().getString(R.string.RSSLink))).get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
@@ -87,15 +101,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         TextView emptyView = findViewById(R.id.emptyView);
-        if (list.get(0).containsValue("NullPointerException") || list.get(0).containsValue("IOException")) {
-            emptyView.setText(getResources().getString(R.string.emptyXmlList));
-            roetgenEventList.setEmptyView(emptyView);
-            return;
-        } else if (list.get(0).containsValue("IllegalArgumentException")) {
-            emptyView.setText(getResources().getString(R.string.internetConnectionError));
-            roetgenEventList.setEmptyView(emptyView);
-            return;
+
+        switch (list.get(0).get("error")){
+            case "java.lang.NullPointerException":
+                emptyView.setText(getResources().getString(R.string.emptyXmlList));
+                roetgenEventList.setEmptyView(emptyView);
+                return;
+            case "java.net.IOException":
+            case "java.lang.IllegalArgumentException":
+                emptyView.setText(getResources().getString(R.string.internetConnectionError));
+                roetgenEventList.setEmptyView(emptyView);
+                return;
+            case "org.xmlpull.v1.XmlPullParserException":
+                emptyView.setText(getResources().getString(R.string.linkHasNoRSSFeed));
+                roetgenEventList.setEmptyView(emptyView);
+                return;
+            case "java.net.MalformedURLException":
+                emptyView.setText(getResources().getString(R.string.linkIsNotValid));
+                roetgenEventList.setEmptyView(emptyView);
+                return;
         }
+
 
         ArrayList<HashMap<String, String>> data = new ArrayList<>();
 
@@ -114,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
         roetgenEventList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(MainActivity.this, appointmentInfo.class);
+                Intent intent = new Intent(MainActivity.this, AppointmentInfoActivity.class);
 
                 intent.putExtra("content", list.get(i).get("content"));
                 intent.putExtra("link", list.get(i).get("link"));
@@ -131,6 +157,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         new MenuInflater(getApplication()).inflate(R.menu.main_menu, menu);
         menu.findItem(R.id.back).setVisible(false);
+        menu.findItem(R.id.option).setVisible(true).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         menu.findItem(R.id.search).setVisible(true).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         return super.onCreateOptionsMenu(menu);
     }
@@ -138,14 +165,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getTitle().equals(getResources().getString(R.string.search))) {
-            popUpList(catagory);
+        switch (item.getItemId()){
+            case R.id.search:
+                popUpFilter(catagory);
+                break;
+            case R.id.option:
+                popUpOption();
+                break;
         }
+
+
         return true;
     }
 
 
-    public void popUpList(String[] category) {
+    public void popUpFilter(String[] category) {
 
         final ArrayList<String> categorySorted = new ArrayList<String>();
         categorySorted.add(0,getResources().getString(R.string.none));
@@ -162,13 +196,52 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+
         final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle(getResources().getString(R.string.search));
+        builder.setNeutralButton(getResources().getString(R.string.back), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+
         builder.setItems(categorySorted.toArray(new String[categorySorted.size()]), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 searchFilter = categorySorted.get(which);
                 recreate();
+            }
+        });
+        builder.show();
+    }
+
+
+    public void popUpOption(){
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.pop_up,null);
+        builder.setView(view);
+        builder.setTitle(getResources().getString(R.string.setLinkOption));
+        final EditText textView = view.findViewById(R.id.popUpTextView);
+        textView.setText(sharedPref.getString("RSSLink",getResources().getString(R.string.RSSLink)));
+        textView.setSelection(textView.getText().length());
+        textView.setVisibility(View.VISIBLE);
+        builder.setNeutralButton(getResources().getString(R.string.back), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                if(textView.getText().toString().isEmpty()) {
+                    editor.remove("RSSLink");
+                    editor.commit();
+                }else {
+                    editor.remove("RSSLink");
+                    editor.putString("RSSLink",textView.getText().toString());
+                    editor.commit();
+                }
+                MainActivity.this.recreate();
             }
         });
         builder.show();
