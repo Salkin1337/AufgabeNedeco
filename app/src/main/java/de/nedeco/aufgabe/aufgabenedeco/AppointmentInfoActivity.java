684cffbe7036ed88f3f81f111a9b854e34ea535c
LordSalkin <niklas@brendt.org>
2018-01-14 17:41:01 +0100

    Update AppointmentInfoActivity.java

--- app/src/main/java/de/nedeco/aufgabe/aufgabenedeco/AppointmentInfoActivity.java
+++ app/src/main/java/de/nedeco/aufgabe/aufgabenedeco/AppointmentInfoActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -24,13 +25,17 @@ import java.net.URL;
  * Created by User on 10.12.2017.
  */
 
+//List title,date,published date,link,description an pictures.
 public class AppointmentInfoActivity extends AppCompatActivity {
 
-    String link;
+    String title;
     String date;
+    String link;
+    String pubDate;
     String content;
     String pic;
     Drawable picDrawable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,31 +46,43 @@ public class AppointmentInfoActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.animationin_rl, R.anim.animationout_rl);
 
 
-        WebView contentView = findViewById(R.id.content);
-        TextView linkView = findViewById(R.id.link);
-        TextView dateView = findViewById(R.id.date);
-        ImageView picView = findViewById(R.id.pictureView);
+        TextView titleView = (TextView)findViewById(R.id.title) ;
+        WebView contentView = (WebView)findViewById(R.id.content);
+        TextView linkView = (TextView)findViewById(R.id.link);
+        TextView pubDateView = (TextView)findViewById(R.id.pubDate);
+        ImageView picView = (ImageView) findViewById(R.id.pictureView);
 
 
-        link = getIntent().getStringExtra("link");
         date = getIntent().getStringExtra("date");
+        title = getIntent().getStringExtra("title");
+        link = getIntent().getStringExtra("link");
+        pubDate = getIntent().getStringExtra("pubDate");
         content = getIntent().getStringExtra("content");
         pic = getIntent().getStringExtra("pic");
 
 
+        titleView.setText(Html.fromHtml(date+"<br>"+title));
+
+
+        for(int i=2;titleView.getLineCount()==i;i++){
+
+        }
+
         try {
+            //Replace the spaces with %20 because in lower verson it cashes for some reason.
             pic = "https" + pic.split("http")[1].replaceAll(" ","%20");
         }catch (ArrayIndexOutOfBoundsException e){
             e.printStackTrace();
         }
 
+        pubDateView.setText(getResources().getString(R.string.published)+pubDate);
 
-        dateView.setText(date);
+
         contentView.getSettings().setJavaScriptEnabled(true);
         contentView.loadDataWithBaseURL(null,"<html>" + content+"</html>", "text/html", "utf-8", null);
         linkView.setText(link);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
         try {
@@ -80,11 +97,12 @@ public class AppointmentInfoActivity extends AppCompatActivity {
         picView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                popUpPicture();
+                popUpPicture(picDrawable);
             }
         });
     }
 
+    // Sets up the Menu Buttons and shows them if needed
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         new MenuInflater(getApplication()).inflate(R.menu.main_menu,menu);
@@ -94,6 +112,7 @@ public class AppointmentInfoActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    //this is for checking which button is pressed filtered by id
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.back){
@@ -102,8 +121,8 @@ public class AppointmentInfoActivity extends AppCompatActivity {
         return true;
     }
 
-    public void popUpPicture(){
-
+    //popUpOption is an alertdialog that shows an ImageView with the given Drawable
+    public void popUpPicture(Drawable picDrawable){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
         View view = inflater.inflate(R.layout.pop_up,null);
