425c01fb2f06e1f218f427e00bcdbb677aac9f82
LordSalkin <niklas@brendt.org>
2018-01-14 17:41:44 +0100

    Update Entry.java

--- app/src/main/java/de/nedeco/aufgabe/aufgabenedeco/Entry.java
+++ app/src/main/java/de/nedeco/aufgabe/aufgabenedeco/Entry.java
@@ -3,24 +3,27 @@ package de.nedeco.aufgabe.aufgabenedeco;
 
 /**
  * Created by User on 09.12.2017.
+ * Date class with getter
  */
 
 public class Entry {
 
     public final String title;
-    public final String link;
-    public final String discription;
     public final String date;
+    public final String link;
+    public final String description;
+    public final String pubDate;
     public final String content;
     public final String pic;
     public final String category;
     public final String error;
 
-    Entry(String title, String description, String link, String date, String content, String pic, String category,String error) {
+    Entry(String title,String date, String description, String link, String pubDate, String content, String pic, String category,String error) {
         this.title = title;
-        this.discription = description;
-        this.link = link;
         this.date = date;
+        this.description = description;
+        this.link = link;
+        this.pubDate = pubDate;
         this.content = content;
         this.pic = pic;
         this.category = category;
@@ -32,16 +35,20 @@ public class Entry {
         return title;
     }
 
+    public String getDate() {
+        return date;
+    }
+
     public String getLink() {
         return link;
     }
 
-    public String getDiscription() {
-        return discription;
+    public String getDescription() {
+        return description;
     }
 
-    public String getDate() {
-        return date;
+    public String getPubDate() {
+        return pubDate;
     }
 
     public String getContent() {
@@ -55,6 +62,7 @@ public class Entry {
     public  String getCategory() {
         return category;
     }
+
     public String getError() {
         return error;
     }
